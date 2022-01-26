018943dce228768bfb7a72ae32f758a66aa3959f
unknown <med@ht.com>
2022-01-26 19:43:45 +0100

    Add GenericArrayList

---
+++ DotNetGenerics/GenericArrayList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DotNetGenerics
+{
+    internal sealed class GenericArrayList
+    {
+        private object[] _items;
+        private int _size;
+
+        public GenericArrayList(int initialCapacity)
+        {
+            _items = new object[initialCapacity];
+        }
+
+        public void Add(object item)
+        {
+            if (_size < _items.Length - 1)
+            {
+                _items[_size] = item;
+                _size++;
+            }
+            else
+            {
+                var newEmployeeCollection = new object[_items.Length * 2];
+                for (int i = 0; i < _items.Length; i++)
+                {
+                    newEmployeeCollection[i] = _items[i];
+                }
+
+                newEmployeeCollection[_items.Length + 1] = item;
+                _size = _items.Length + 1;
+                _items = newEmployeeCollection;
+            }
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _size)
+                    throw new IndexOutOfRangeException();
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _size)
+                    throw new IndexOutOfRangeException();
+                _items[index] = value;
+            }
+        }
+    }
+}
--- DotNetGenerics/Program.cs
+++ DotNetGenerics/Program.cs
@@ -2,11 +2,15 @@
 
 namespace DotNetGenerics
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //The problem with object GenericArrayList is typeSafety
+            var list = new GenericArrayList(5);
+            list.Add(new Employee("Med", 1000, Team.Development));
+            list.Add(new Freelancer("Ht", 2000, Team.Operation)); // very bad
+            list.Add(3); // yes, it possible ! 
         }
     }
 }
